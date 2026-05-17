6b5824c14536fca1647b5a752e1b13dd53cb2576
Ameer <ameergamez06@gmail.com>
2026-05-17 03:42:03 +0100

    try: cloudflare tunnel for backend

--- frontend/route.js
+++ frontend/route.js
@@ -1,2 +1,2 @@
 const IS_PROD = true;
-export const ROUTE_BACKEND_SERVER = (IS_PROD ? "190.213.89.168" : "127.0.0.1") + ":8000";
+export const ROUTE_BACKEND_SERVER = (IS_PROD ? "stocks-succeed-exciting-hostels.trycloudflare.com" : "127.0.0.1:8000");
